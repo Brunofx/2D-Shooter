723af1aaca90f69b3801d5f3dcb1e3346501a9de
Bruno <bruno.oks1@gmail.com>
2020-06-25 17:46:30 -0300

    v0.05
    - Ajustado waves de enemies
    - Criado sistema de dano
    - Criado destroy quando a vida chega a zero

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 100; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        DamageDealer damage = collision.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damage);
+    }
+
+    private void ProcessHit(DamageDealer damage) {
+        health = health - damage.GetDamage();
+
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
